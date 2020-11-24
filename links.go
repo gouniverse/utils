fce7168e70e607cd38e064aef43c6940ddb1114a
Milan Lesichkov <lesichkovm@gmail.com>
2020-11-24 18:57:15 +0000

    Create links.go

---
+++ links.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"os"
+	"unicode"
+
+	"golang.org/x/text/unicode/norm"
+)
+
+// LinkWebsite returns a URL to the current website
+func LinkWebsite() string {
+	return ""
+	appURL := AppURL()
+	appPort := AppPort()
+	appAddress := appURL
+	if appPort != "80" {
+		appAddress += ":" + appPort
+	}
+	appProtocol := "https"
+	if appURL == "127.0.0.1" || appURL == "localhost" {
+		appProtocol = "http"
+	}
+	return appProtocol + "://" + appAddress
+}
