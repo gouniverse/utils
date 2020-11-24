9d9a52d1abbcfb7710c7cab37e88df1df1d5b721
Milan Lesichkov <lesichkovm@gmail.com>
2020-11-24 18:45:55 +0000

    Update app.go

--- app.go
+++ app.go
@@ -40,6 +40,42 @@ func AppURL() string {
 	return appURL
 }
 
+// DbDriver returns the driver of the database
+func DbDriver() string {
+	appURL := os.Getenv("DB_CONNECTION")
+	return appURL
+}
+
+// DbHost return the host of the database
+func DbHost() string {
+	appURL := os.Getenv("DB_HOST")
+	return appURL
+}
+
+// DbPort return the port of the database
+func DbPort() string {
+	appURL := os.Getenv("DB_PORT")
+	return appURL
+}
+
+// DbDatabase return the name of the database
+func DbDatabase() string {
+	appURL := os.Getenv("DB_DATABASE")
+	return appURL
+}
+
+// DbUsername return the username of the database
+func DbUsername() string {
+	appURL := os.Getenv("DB_USERNAME")
+	return appURL
+}
+
+// DbPassword return the password for the database
+func DbPassword() string {
+	appURL := os.Getenv("DB_PASSWORD")
+	return appURL
+}
+
 // EmailFromAddress return the URL for the app
 func EmailFromAddress() string {
 	emailFromAddress := os.Getenv("EMAIL_FROM_ADDRESS")
