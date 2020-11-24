36ab779824f82ae81db332fc46e7560efc6e580f
Milan Lesichkov <lesichkovm@gmail.com>
2020-11-24 08:15:34 +0000

    Create arrays.go

---
+++ arrays.go
@@ -0,0 +1,24 @@
+package utils
+
+import "reflect"
+
+// InArray checks whether a value is in the array
+func InArray(val interface{}, array interface{}) (exists bool, index int) {
+	exists = false
+	index = -1
+
+	switch reflect.TypeOf(array).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(array)
+
+		for i := 0; i < s.Len(); i++ {
+			if reflect.DeepEqual(val, s.Index(i).Interface()) == true {
+				index = i
+				exists = true
+				return
+			}
+		}
+	}
+
+	return
+}
