9f6ec3bbaccdcd9f3bf20d80925e34803837b310
Milan Lesichkov <lesichkovm@gmail.com>
2020-11-25 08:13:18 +0000

    Update arrays.go

--- arrays.go
+++ arrays.go
@@ -22,3 +22,16 @@ func ArrayContains(array interface{}, val interface{}) (exists bool, index int) 
 
 	return
 }
+
+// IsStringArrayEqual checks whether 2 string arrays are the same
+func IsStringArrayEqual(a, b []string) bool {
+    if len(a) != len(b) {
+        return false
+    }
+    for i := 0; i < len(a); i++ {
+        if a[i] != b[i] {
+            return false
+        }
+    }
+    return true
+}
