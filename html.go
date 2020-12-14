379eb200a3e6d8df08ae4a4df13099d7b0d4f086
Milan Lesichkov <lesichkovm@gmail.com>
2020-12-14 07:54:44 +0000

    Update html.go

--- html.go
+++ html.go
@@ -38,23 +38,6 @@ func MinHTML(htmlString string) (string, error) {
 	return m.String("text/html", htmlString)
 }
 
-// Req returns a POST or GET key, or default if not exists
-func Req(r *http.Request, key string, defaultValue string) string {
-	postValue := r.FormValue(key)
-
-	if len(postValue) > 0 {
-		return postValue
-	}
-
-	getValue := r.URL.Query().Get(key)
-
-	if len(getValue) > 0 {
-		return getValue
-	}
-
-	return defaultValue
-}
-
 // ScriptsHTML the HTML from scripts string
 func ScriptsHTML(str string) string {
 	scripts := strings.Split(str, ",")
