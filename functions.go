c708921a988c5a2366d9e7579d518e130430f0c2
Milan Lesichkov <lesichkovm@gmail.com>
2020-12-14 07:57:25 +0000

    Update functions.go

--- functions.go
+++ functions.go
@@ -1,41 +1,11 @@
 package utils
 
 import (
-	"encoding/json"
-	"net/http"
 	"strconv"
-
-	"github.com/gouniverse/api"
 )
 
 // IsNumeric checks if a string is numeric
 func IsNumeric(s string) bool {
 	_, err := strconv.ParseFloat(s, 64)
 	return err == nil
-}
-
-// Req returns a POST or GET variable, or default if not exists
-func Req(r *http.Request, key string, defaultValue string) string {
-	postValue := r.FormValue(key)
-
-	if len(postValue) > 0 {
-		return postValue
-	}
-
-	getValue := r.URL.Query().Get(key)
-
-	if len(getValue) > 0 {
-		return getValue
-	}
-
-	return defaultValue
-}
-
-// Respond returns an API response as JSON
-func Respond(w http.ResponseWriter, response api.Response) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	//json.NewEncoder(w).Encode(api.Success("API Working on " + environment))
-	bytes, _ := json.Marshal(response)
-	w.Write(bytes)
 }
