657fd469264a711e11f232beec6a9cd42bc5a89a
Milan Lesichkov <lesichkovm@gmail.com>
2020-12-29 02:21:47 +0000

    Update strings_test.go

--- strings_test.go
+++ strings_test.go
@@ -1,1 +1,14 @@
 package utils
+
+import (
+	"testing"
+)
+
+func TestToString(t *testing.T) {
+	if ToString(1) != "1" {
+        t.Error("Conversion int to string failed")
+	}
+	if ToString("Hello.txt") != "Hello.txt" {
+        t.Error("Conversion string to string failed")
+	}
+}
