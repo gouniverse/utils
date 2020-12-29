41ae8f217ea7fe0d2cbc0825ecaa1c2d27a83d29
Milan Lesichkov <lesichkovm@gmail.com>
2020-12-29 02:01:42 +0000

    Update strings.go

--- strings.go
+++ strings.go
@@ -79,3 +79,23 @@ func Slugify(s string, replaceWith rune) string {
 
 	return string(buf)
 }
+
+// ToString converts an interface to string
+func ToString(v interface{}) string {
+	switch v := v.(type) {
+	case string:
+		return v
+
+	case []byte:
+		return btos(v)
+
+	case int:
+		return strconv.Itoa(v)
+
+	case float64:
+		return strconv.FormatFloat(v, 'f', 4, 64)
+
+	default:
+		return fmt.Sprint(v)
+	}
+}
