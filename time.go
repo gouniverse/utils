2911f8b6db7bcd40346e22cfbe38bd3a62c41eac
Milan Lesichkov <lesichkovm@gmail.com>
2020-12-29 03:06:54 +0000

    Create time.go

---
+++ time.go
@@ -0,0 +1,15 @@
+package utils
+
+import(
+	"time"
+)
+
+// StrToTimeUnix converts sting to time
+func StrToTimeUnix(str string) (int64, error) {
+	layout := "2006-01-02 15:04:05"
+	t, err := time.Parse(layout, str)
+	if err != nil {
+		return 0, err
+	}
+	return t.Unix(), nil
+}
