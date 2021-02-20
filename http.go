20e17980bb83ef994c5ee0b6613be9afa489145a
Milan Lesichkov <lesichkovm@gmail.com>
2021-02-20 05:07:12 +0000

    Update http.go

--- http.go
+++ http.go
@@ -7,6 +7,28 @@ import (
 	"github.com/gouniverse/api"
 )
 
+// IP gets the IP address for the user
+func IP(r *http.Request) string {
+	//Get IP from the X-REAL-IP header
+	realIP := r.Header.Get("X-REAL-IP")
+	if realIP != nil {
+		return realIP, nil
+	}
+	
+	//Get IP from X-FORWARDED-FOR header
+	forwarded := r.Header.Get("X-FORWARDED-FOR")
+	if forwarded != "" {
+		splitIps := strings.Split(forwarded, ",")
+		for _, ip := range splitIps {
+		    return ip, nil
+		}
+		return forwarded
+	}
+	
+	//Get IP from RemoteAddr
+	return r.RemoteAddr
+}
+
 // Req returns a POST or GET key, or default if not exists
 func Req(r *http.Request, key string, defaultValue string) string {
 	postValue := r.FormValue(key)
