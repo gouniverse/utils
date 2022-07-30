e1c1aab8465c72782aa5253ca443d49871d3259f
Milan Lesichkov <lesichkovm@gmail.com>
2022-07-30 17:41:02 +0100

    Update ArrayContains.go

--- ArrayContains.go
+++ ArrayContains.go
@@ -22,3 +22,18 @@ func ArrayContains(array interface{}, val interface{}) (exists bool, index int) 
 
 	return
 }
+
+// TODO. Update ArrayContents to use this style as its more readable
+func InArray(val interface{}, array interface{}) (index int) {
+	values := reflect.ValueOf(array)
+
+	if reflect.TypeOf(array).Kind() == reflect.Slice || values.Len() > 0 {
+		for i := 0; i < values.Len(); i++ {
+			if reflect.DeepEqual(val, values.Index(i).Interface()) {
+				return i
+			}
+		}
+	}
+
+	return -1
+}
