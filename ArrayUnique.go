dabf7e5394d50f9c3293132f2d075f93d2229269
Milan Lesichkov <lesichkovm@gmail.com>
2022-08-09 17:54:30 +0100

    Create ArrayUnique.go

---
+++ ArrayUnique.go
@@ -0,0 +1,14 @@
+package utils
+
+// TODO. ArrayUnique removes duplicate strings from a string slice
+func ArrayUnique(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
