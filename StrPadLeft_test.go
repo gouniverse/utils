4c0f09723e979f4687fd61d837f4ad3dcb2a4f1c
Milan Lesichkov <lesichkovm@gmail.com>
2022-08-13 06:14:16 +0100

    Update StrPadLeft_test.go

--- StrPadLeft_test.go
+++ StrPadLeft_test.go
@@ -1,1 +1,14 @@
+package utils
 
+import "testing"
+
+func TestStrPadLeft(t *testing.T) {
+	str := "1"
+	expected := "01"
+
+	result := StrPadLeft(str, 2, "0")
+
+	if result != expected {
+		t.Error("StrPadLeft does not return expected result, but %s", result)
+	}
+}
