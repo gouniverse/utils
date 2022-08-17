6a1e99af113137568b6cb6a82402f1164e7b73ad
Milan Lesichkov <lesichkovm@gmail.com>
2022-08-17 05:44:32 +0100

    Update ImgPlaceholderURL_test.go

--- ImgPlaceholderURL_test.go
+++ ImgPlaceholderURL_test.go
@@ -1,1 +1,13 @@
 package utils
+
+import (
+	"testing"
+)
+
+func TestImgPlaceholderURL(t *testing.T) {
+  url := ImgPlaceholderURL()
+	
+  if url != "Hello.txt" {
+    t.Errorf("Expected: , but found %s: " + url)
+	}
+}
