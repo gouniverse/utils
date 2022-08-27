2aaafe4cd700b002a5ac7afac3f5f23dbc9ad3bb
Milan Lesichkov <lesichkovm@gmail.com>
2022-08-28 00:51:45 +0100

    Update JSONDecode.go

--- JSONDecode.go
+++ JSONDecode.go
@@ -1,7 +1,5 @@
 package utils
 
-import "encoding/json"
-
 // JSONDecode shortcode for FromJSON
 func JSONDecode(jsonString string, valueDefault interface{}) (interface{}, error) {
 	return FromJSON(jsonString, valueDefault)
