b6b4e389cbfaa9e94f9ac2e6e45522e6c6f8c11d
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-13 10:41:26 +0100

    Update ToString.go

--- ToString.go
+++ ToString.go
@@ -17,6 +17,24 @@ func ToString(v interface{}) string {
 
 	case int:
 		return strconv.Itoa(v)
+	case int8:
+		return strconv.FormatInt(int64(v), 10)
+	case int16:
+		return strconv.FormatInt(int64(v), 10)
+	case int32:
+		return strconv.FormatInt(int64(v), 10)
+	case int64:
+		return strconv.FormatInt(v, 10)
+	case uint:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint64:
+		return strconv.FormatUint(v, 10)
 
 	case float64:
 		return strconv.FormatFloat(v, 'f', 4, 64)
