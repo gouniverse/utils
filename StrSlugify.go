9818b21d493a06c2070dbd57b38dd71b3b907a32
Milan Lesichkov <lesichkovm@gmail.com>
2023-06-25 22:23:10 +0100

    Update StrSlugify.go

--- StrSlugify.go
+++ StrSlugify.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bytes"
 	"unicode"
 
 	"golang.org/x/text/unicode/norm"
@@ -12,6 +13,8 @@ import (
 // codepoints that do not have combining marks or a lower-cased variant will be
 // passed through unaltered.
 func StrSlugify(s string, replaceWith rune) string {
+	s = cyrillicToLatin(s)
+
 	// The "safe" set of characters.
 	alphanum := &unicode.RangeTable{
 		R16: []unicode.Range16{
@@ -50,4 +53,79 @@ func StrSlugify(s string, replaceWith rune) string {
 	}
 
 	return string(buf)
+}
+
+func cyrillicToLatin(s string) string {
+	var sub = map[rune]string{
+		'А': "A",
+		'Б': "B",
+		'В': "V",
+		'Г': "G",
+		'Д': "D",
+		'Е': "E",
+		'Ж': "Zh",
+		'З': "Z",
+		'И': "I",
+		'Й': "Y",
+		'К': "K",
+		'Л': "L",
+		'М': "M",
+		'Н': "N",
+		'О': "O",
+		'П': "P",
+		'Р': "R",
+		'С': "S",
+		'Т': "T",
+		'У': "U",
+		'Ф': "F",
+		'Х': "H",
+		'Ц': "Ts",
+		'Ч': "Ch",
+		'Ш': "Sh",
+		'Щ': "Sh",
+		'Ъ': "A",
+		'Ь': "Y",
+		'Ю': "Yu",
+		'Я': "Ya",
+		'а': "a",
+		'б': "b",
+		'в': "v",
+		'г': "g",
+		'д': "d",
+		'е': "e",
+		'ж': "zh",
+		'з': "z",
+		'и': "i",
+		'й': "y",
+		'к': "k",
+		'л': "l",
+		'м': "m",
+		'н': "n",
+		'о': "o",
+		'п': "p",
+		'р': "r",
+		'с': "s",
+		'т': "t",
+		'у': "u",
+		'ф': "f",
+		'х': "h",
+		'ц': "ts",
+		'ч': "ch",
+		'ш': "sh",
+		'щ': "sht",
+		'ъ': "a",
+		'ь': "y",
+		'ю': "yu",
+		'я': "ya",
+	}
+
+	var buf bytes.Buffer
+	for _, c := range s {
+		if d, ok := sub[c]; ok {
+			buf.WriteString(d)
+		} else {
+			buf.WriteRune(c)
+		}
+	}
+	return buf.String()
 }
