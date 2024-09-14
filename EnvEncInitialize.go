5bb6dd02406cbb9034a42f953baac30bf2d3dc3e
Milan Lesichkov <lesichkovm@gmail.com>
2024-09-14 06:42:25 +0100

    Update EnvEncInitialize.go

--- EnvEncInitialize.go
+++ EnvEncInitialize.go
@@ -1,25 +1,35 @@
 package utils
 
 import (
+	"errors"
 	"log"
 	"os"
 
 	"github.com/gouniverse/envenc"
 )
 
-func EnvEncInitialize(password string, vaultFilePath string) {
+func EnvEncInitialize(password string, vaultFilePath string) error {
 	if vaultFilePath == "" {
 		vaultFilePath = ".env.vault"
 	}
 
-	if FileExists(vaultFilePath) {
-		keys, err := envenc.EnvList(vaultFilePath, password)
-		if err != nil {
-			log.Println(err.Error())
-		}
+	if !FileExists(vaultFilePath) {
+		return errors.New("Vault file not found: " + vaultFilePath)
+	}
 
-		for k, v := range keys {
-			os.Setenv(k, v)
+	keys, err := envenc.EnvList(vaultFilePath, password)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	for k, v := range keys {
+		err := os.Setenv(k, v)
+
+		if err != nil {
+			return err
 		}
 	}
+
+	return nil
 }
